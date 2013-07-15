96dd4291cc05b8ad4395e905bd6afa67f12db22c
semasad <semasad@gmail.com>
2013-07-14 21:21:01 -0400

    Update
    Full update of the site.

--- code.js
+++ code.js
@@ -1,74 +1,12 @@
-$(document).on("ready", inicio);
 
-function inicio () {
-$('#job').hide(0);
-$('#contact').hide(0);
-$('#details').hide(0);
-$('.know').hide(0);
-$('.tools ').hide(0);
-$('.certifications_content').hide(0);
-$('#intro .button').on("click" , show_job);
-$('#job .button').on("click" , show_contact);
-$('#details .button').on("click" , show_contact);
-$('#job .back').on("click" , back);
-$('.more').on("click" , show_details);
-$('#details .back_b').on("click" , back_b);
-$('#contact .back_b').on("click" , back_b);
-$('.skills h4').on("click" , show_skills);
-$('.certifications h4').on("click" , show_certifications);
-$('h6').on("click" , show_contact);
-$('.details').on("click" , show_details);
+$(document).on("ready", start);
 
-}
+function start () {
 
-function show_job () {
+	$("article:nth-child(2)").highlight("+");
+	$("article:nth-child(3)").highlight("+");
+	$("article:nth-child(4)").highlight("+");
+	$("article i").highlight("+");
+	$("h3").highlight("+");
 
-	$('#intro').hide(300);
-	$('#job').show(300);
-
-}
-
-function show_contact () {
-
- 	$('#intro').hide(0);
-	$('#job').hide(300);
-	$('#contact').show(300);
-	$('#details').hide(100);
-	$('h6').hide(100);
-
-}
-function back () {
-
-	$('#job').hide(300);
-	$('#details').hide(300);
-	$('#intro').show(300);
-	$('h6').show(100);
-}
-function back_b () {
-	$('#contact').hide(300);
-	$('#job').show(300);
-	$('#details').hide(300);
-	$('h6').show(100);
-
-}
- function show_skills () {
-
-
-	$('.know').toggle(300);
-	$('.tools').toggle(300);
-	$('h6').show(100);
-
-}
-
- function show_certifications () {
- 	$('.certifications_content').toggle(300);
-
-}
-
-
-function show_details () {
-
-$('#job').hide(300);
-$('#contact').hide(300);
-$('#details').show(300);
 }
